f76903c93c5a58bc488851337b51812ca9157be0
Lukáš Litvan <lukas.litvan@gmail.com>
2022-02-26 15:36:52 +0100

    Add task 3 code, update task 3 readme

--- 03/README.md
+++ 03/README.md
@@ -1,1 +1,16 @@
 # Task 3
+
+## Build image
+```
+docker build -t hello-service-image ./src
+```
+
+## Run container
+```
+docker run -dp 8888:8080 --name hello-service hello-service-image
+```
+
+## Test with curl
+```
+curl localhost:8888/John
+```
---
+++ 03/src/service.js
@@ -0,0 +1,24 @@
+const http = require("http");
+
+const hostname = '0.0.0.0';
+const port = 8080;
+
+http.createServer((req, res) => {
+
+  if ((id = req.url.match("^/([A-Za-z0-9]+)$"))) {
+    if (req.method == "GET") {
+      res.writeHead(200, {
+        'Content-Type': 'text/plain'
+      });
+      res.end('Hello ' + id[1]);
+    }
+  } else {
+    res.writeHead(404, {
+      'Content-Type': 'text/plain'
+    });
+    res.end('404 Not Found');
+  }
+
+}).listen(port, hostname, () => {
+  console.log(`Hello service running at http://${hostname}:${port}/`);
+});
